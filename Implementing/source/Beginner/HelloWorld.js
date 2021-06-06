361ec5aba7f304b2bfce647bb301b1b1635ec297
ToanNB <ngbatoan22@gmail.com>
2021-06-06 14:49:00 +0700

    Hello worlds

---
+++ Implementing/source/Beginner/HelloWorld.js
@@ -0,0 +1,49 @@
+console.log('Started!!!!!!');
+
+/*
+    Operators
+*/
+
+// Arithmetic operators
+console.log("===========Arithmetic operators==============");
+
+function sum(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function division(a, b) {
+    return a / b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function modulus(a, b) {
+    return a % b;
+}
+
+console.log(sum(10, 1));
+console.log(minus(10, 5));
+console.log(division(10, 30));
+console.log(multiplication(5, 6));
+console.log(modulus(5, 3));
+
+// Comparison operators
+console.log("===========Comparison operators=========");
+console.log(5 > 4);
+console.log("z" > "toi");
+var a = 1;
+var b = '1';
+console.log(a === b);
+
+//Logical Operators
+
+//Bitwise Operators
+
+
+console.log('Successfully');
